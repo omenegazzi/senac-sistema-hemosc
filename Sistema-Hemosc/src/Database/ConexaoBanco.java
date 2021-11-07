f016f5bbd95887694ca727083d6f32911ed92c95
Diego <omenegazzi@gmail.com>
2021-11-06 23:11:09 -0200

    Alterado pacotes e Criado conexão com o banco de dados Mysql.

---
+++ Sistema-Hemosc/src/Database/ConexaoBanco.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Database;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author diegomenegazzi
+ */
+public class ConexaoBanco {
+    public static Connection conectaBanco(){
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver").newInstance();
+            return DriverManager.getConnection("jdbc:mysql://localhost/db_hemosc?"
+                            + "user=root&password=root");     
+        } catch (ClassNotFoundException | IllegalAccessException | InstantiationException | SQLException ex) {
+            throw new RuntimeException("Erro ao conectar no banco de dados", ex);
+            
+        }   
+    }
+}
