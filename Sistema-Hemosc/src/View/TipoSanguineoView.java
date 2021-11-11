345b30518f8696050008e3d9dbfadc900d27c309
Isaias <isaias@exemplo.br>
2021-11-11 15:43:55 -0200

    #40 - Desenvolver função pesquisar tipos sanguíneos pelo campo pesquisar.

--- Sistema-Hemosc/src/View/TipoSanguineoView.java
+++ Sistema-Hemosc/src/View/TipoSanguineoView.java
@@ -5,6 +5,9 @@
  */
 package View;
 
+import Model.TipoSanguineo;
+import javax.swing.table.DefaultTableModel;
+
 /**
  *
  * @author guest01
@@ -17,7 +20,26 @@ public class TipoSanguineoView extends javax.swing.JFrame {
     public TipoSanguineoView() {
         initComponents();
     }
-
+    
+    public void pesquisaDados() {
+        
+        DefaultTableModel tabela = (DefaultTableModel) tTipoSanguineo.getModel(); 
+       
+        TipoSanguineoDAO dao = new TipoSanguienoDAO();
+        tabela.setNumRows(0);
+        
+        for(TipoSanguineo ts : dao.pesquisar(tfPesquisar.getText())){
+            tabela.addRow(new Object[]{
+               ts.getId_TipoSanguineo(),
+               ts.getDescricao(),
+               ts.getFator_rh(),
+               ts.getEstoque(),
+               ts.getEstoque_minimo() 
+            });
+        }
+        
+    }
+   
     /**
      * This method is called from within the constructor to initialize the form.
      * WARNING: Do NOT modify this code. The content of this method is always
@@ -45,10 +67,10 @@ public class TipoSanguineoView extends javax.swing.JFrame {
         jButton3 = new javax.swing.JButton();
         jButton4 = new javax.swing.JButton();
         jButton5 = new javax.swing.JButton();
-        jButton6 = new javax.swing.JButton();
-        jTextField5 = new javax.swing.JTextField();
+        bPesquisar = new javax.swing.JButton();
+        tfPesquisar = new javax.swing.JTextField();
         jScrollPane2 = new javax.swing.JScrollPane();
-        jTable2 = new javax.swing.JTable();
+        tTipoSanguineo = new javax.swing.JTable();
 
         jTable1.setModel(new javax.swing.table.DefaultTableModel(
             new Object [][] {
@@ -89,9 +111,14 @@ public class TipoSanguineoView extends javax.swing.JFrame {
 
         jButton5.setText("Fechar");
 
-        jButton6.setText("Pesquisar");
+        bPesquisar.setText("Pesquisar");
+        bPesquisar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                bPesquisarActionPerformed(evt);
+            }
+        });
 
-        jTable2.setModel(new javax.swing.table.DefaultTableModel(
+        tTipoSanguineo.setModel(new javax.swing.table.DefaultTableModel(
             new Object [][] {
 
             },
@@ -99,7 +126,7 @@ public class TipoSanguineoView extends javax.swing.JFrame {
                 "Código", "Descrição", "Estoque", "Estoque mínimo", "Fator RH"
             }
         ));
-        jScrollPane2.setViewportView(jTable2);
+        jScrollPane2.setViewportView(tTipoSanguineo);
 
         javax.swing.GroupLayout jPanel2Layout = new javax.swing.GroupLayout(jPanel2);
         jPanel2.setLayout(jPanel2Layout);
@@ -139,9 +166,9 @@ public class TipoSanguineoView extends javax.swing.JFrame {
                                     .addComponent(jButton5)))
                             .addGroup(jPanel2Layout.createSequentialGroup()
                                 .addContainerGap()
-                                .addComponent(jTextField5, javax.swing.GroupLayout.PREFERRED_SIZE, 396, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addComponent(tfPesquisar, javax.swing.GroupLayout.PREFERRED_SIZE, 396, javax.swing.GroupLayout.PREFERRED_SIZE)
                                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                                .addComponent(jButton6)))
+                                .addComponent(bPesquisar)))
                         .addGap(0, 12, Short.MAX_VALUE)))
                 .addGap(80, 80, 80))
             .addGroup(jPanel2Layout.createSequentialGroup()
@@ -188,8 +215,8 @@ public class TipoSanguineoView extends javax.swing.JFrame {
                             .addComponent(jButton4)
                             .addComponent(jButton5))
                         .addGap(31, 31, 31)
-                        .addComponent(jButton6))
-                    .addComponent(jTextField5, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addComponent(bPesquisar))
+                    .addComponent(tfPesquisar, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addGap(18, 18, 18)
                 .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, 113, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addContainerGap(33, Short.MAX_VALUE))
@@ -211,6 +238,12 @@ public class TipoSanguineoView extends javax.swing.JFrame {
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void bPesquisarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_bPesquisarActionPerformed
+        // TODO add your handling code here:
+        
+        pesquisaDados();
+    }//GEN-LAST:event_bPesquisarActionPerformed
 
     /**
      * @param args the command line arguments
@@ -248,12 +281,12 @@ public class TipoSanguineoView extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton bPesquisar;
     private javax.swing.JButton jButton1;
     private javax.swing.JButton jButton2;
     private javax.swing.JButton jButton3;
     private javax.swing.JButton jButton4;
     private javax.swing.JButton jButton5;
-    private javax.swing.JButton jButton6;
     private javax.swing.JComboBox<String> jComboBox1;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
@@ -264,11 +297,11 @@ public class TipoSanguineoView extends javax.swing.JFrame {
     private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JScrollPane jScrollPane2;
     private javax.swing.JTable jTable1;
-    private javax.swing.JTable jTable2;
     private javax.swing.JTextField jTextField1;
     private javax.swing.JTextField jTextField2;
     private javax.swing.JTextField jTextField3;
     private javax.swing.JTextField jTextField4;
-    private javax.swing.JTextField jTextField5;
+    private javax.swing.JTable tTipoSanguineo;
+    private javax.swing.JTextField tfPesquisar;
     // End of variables declaration//GEN-END:variables
 }
