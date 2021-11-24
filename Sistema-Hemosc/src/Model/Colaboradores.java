5a21dba26b8bc3663cc53caca166122afe32e9dd
Marcelo do Nascimento <nascimentomarcelo440@gmail.com>
2021-11-24 17:39:17 -0200

    Criada Função Pesquisar em DoacoesDao e DoacoesView

--- Sistema-Hemosc/src/Model/Colaboradores.java
+++ Sistema-Hemosc/src/Model/Colaboradores.java
@@ -59,7 +59,7 @@ private String funcao;
 
     @Override
     public String toString() {
-        return super.toString(); //To change body of generated methods, choose Tools | Templates.
+        return getNome();
     }
     
     
