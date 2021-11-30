c663798e9c71d7e5402548c4b56b7e1175b34068
Gabriel Balestrin <balestringabriel10@gmail.com>
2021-11-30 15:23:03 -0200

    realizado alterações

--- Sistema-Hemosc/src/Controller_DAO/DoacoesDao.java
+++ Sistema-Hemosc/src/Controller_DAO/DoacoesDao.java
@@ -19,49 +19,91 @@ import java.util.logging.Logger;
  * @author marcelo.nascimento1
  */
 public class DoacoesDao {
-    
-    public List<Doacoes> pesquisar(String texto){
-        
+
+    public List<Doacoes> pesquisar(String texto) {
+
         Connection conn = ConexaoBanco.conectaBanco();
-        
+
         PreparedStatement stmt = null;
         ResultSet rs = null;
-        
+
         List<Doacoes> doacoes = new ArrayList<>();
-        
+
         try {
-            stmt = conn.prepareStatement("SELECT id_doacao,data,doadores.nome AS nome_doador, tipos_sanguineos.descricao,tipos_sanguineos.fator_rh, colaboradores.nome AS nome_colaborador FROM doacoes \n" +
-                                        "INNER JOIN doadores ON (doadores.id_doador = doacoes.id_doador) \n" +
-                                        "INNER JOIN tipos_sanguineos ON (tipos_sanguineos.id_tipo_sanguineo = doacoes.id_tipo_sanguineo)\n" +
-                                        "INNER JOIN colaboradores ON (colaboradores.id_colaborador = doacoes.id_colaborador)\n" +
-                                        "WHERE id_doador like (?)"); 
-            stmt.setString(1,"%"+texto+"%");
-            
+            stmt = conn.prepareStatement("SELECT id_doacao,data,doadores.nome AS nome_doador, tipos_sanguineos.descricao,tipos_sanguineos.fator_rh, colaboradores.nome AS nome_colaborador FROM doacoes \n"
+                    + "INNER JOIN doadores ON (doadores.id_doador = doacoes.id_doador) \n"
+                    + "INNER JOIN tipos_sanguineos ON (tipos_sanguineos.id_tipo_sanguineo = doacoes.id_tipo_sanguineo)\n"
+                    + "INNER JOIN colaboradores ON (colaboradores.id_colaborador = doacoes.id_colaborador)\n"
+                    + "WHERE id_doador like (?)");
+            stmt.setString(1, "%" + texto + "%");
+
             rs = stmt.executeQuery();
-        
-            while (rs.next()){
+
+            while (rs.next()) {
                 Doacoes D = new Doacoes();
                 D.setId_doacao(rs.getInt("id_doacao"));
                 D.setData(rs.getDate("data"));
-                
+
                 Doadores doar = new Doadores();
-                doar.setNome(rs.getString("nome_doador")); 
+                doar.setNome(rs.getString("nome_doador"));
                 D.setId_doador(doar);
-                
+
                 TipoSanguineo T = new TipoSanguineo();
-                T.setDescricao(rs.getString("tipo_sanguineo")); 
+                T.setDescricao(rs.getString("tipo_sanguineo"));
                 D.setId_tipo_sanguineo(T);
-                
+
                 Colaboradores C = new Colaboradores();
-                C.setNome(rs.getString("nome_colaborador")); 
+                C.setNome(rs.getString("nome_colaborador"));
                 D.setId_colaborador(C);
-           
+
                 doacoes.add(D);
             }
         } catch (SQLException ex) {
             Logger.getLogger(FuncoesDao.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+        return doacoes;
+    }
+
+    public List<Doacoes> listar() {
+
+        Connection conn = ConexaoBanco.conectaBanco();
+
+        PreparedStatement stmt = null;
+        ResultSet rs = null;
+
+        List<Doacoes> doacoes = new ArrayList<>();
+
+        try {
+            stmt = conn.prepareStatement("select id_doacao, doadores.nome, tipos_sanguineos.descricao, colaboradores.nome from doacoes\n"
+                    + "inner join doadores on (doadores.id_doador = doacoes.id_doador)\n"
+                    + "inner join tipos_sanguineos on (tipos_sanguineos.id_tipo_sanguineo = doacoes.id_tipo_sanguineo)\n"
+                    + "inner join colaboradores on (colaboradores.id_colaborador = doacoes.id_colaborador)");
+            rs = stmt.executeQuery();
+
+            while (rs.next()) {
+                Doacoes d = new Doacoes();
+                d.setId_doacao(rs.getInt("id_doacao"));
+                d.setData(rs.getDate("data"));
+
+                Doadores doa = new Doadores();
+                doa.setId_doador(rs.getInt("id_doador"));
+                d.setId_doador(doa);
+
+                TipoSanguineo Ts = new TipoSanguineo();
+                Ts.setId_TipoSanguineo(rs.getInt("id_tipo_sanguineo"));
+                d.setId_tipo_sanguineo(Ts);
+
+                Colaboradores Col = new Colaboradores();
+                Col.setId_colaborador(rs.getInt("id_colaborador"));
+                d.setId_colaborador(Col);
+
+                doacoes.add(d);
             }
-        
+        } catch (SQLException ex) {
+            Logger.getLogger(DoadoresDao.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
         return doacoes;
     }
 }
